8fb86bbd50d7990a6f871f0f81af3324f6aa8afe
Andrea Redel <andrearedel@MacBook-Air-de-Andrea.local>
2025-07-18 00:20:04 +0200

    nueva versión

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 set -e
 
 # Descargar e instalar Quarto
-curl -LO https://github.com/quarto-dev/quarto-cli/releases/latest/download/quarto-linux-amd64.tar.gz
-tar -xvzf quarto-linux-amd64.tar.gz
+curl -LO https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.550/quarto-1.4.550-linux-amd64.tar.gz
+tar -xvzf quarto-1.4.550-linux-amd64.tar.gz
 export PATH=$PWD/quarto/bin:$PATH
 
 # Verificar que Quarto funciona
